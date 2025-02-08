d6779b2bfd41bc0b363b2489fd1da5b6f0f13905
Ivan <statkevich.ivan999@gmail.com>
2025-02-08 14:10:18 +0200

    Add handler function to scheduled_function for basic HTTP request handling

---
+++ scheduled_function/main.py
@@ -0,0 +1,25 @@
+import json
+
+
+def handler(event):
+    path = event.get("rawPath", "")
+    method = event.get("requestContext", {}).get("http", {}).get("method", "")
+
+    if path == "/" and method == "GET":
+        return {
+            "statusCode":
+            200,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body":
+            json.dumps({"message": "Hello from AWS Lambda with pure Python!"})
+        }
+    else:
+        return {
+            "statusCode": 404,
+            "headers": {
+                "Content-Type": "application/json"
+            },
+            "body": json.dumps({"error": "Not Found"})
+        }
