4c3bc5edb21bcfca37c7a06e0cf46ec85e388d20
Ivan <statkevich.ivan999@gmail.com>
2025-02-09 19:24:46 +0200

    Refactor sentiment analysis function to save results in DynamoDB and retrieve Reddit credentials from AWS Secrets Manager

--- collect_data_function/main.py
+++ collect_data_function/main.py
@@ -1,25 +1,35 @@
-import time
 import praw
 import json
+import boto3
 from vaderSentiment.vaderSentiment import SentimentIntensityAnalyzer
+from decimal import Decimal, ROUND_HALF_UP
+from datetime import datetime
+
+secret_name = "sentiment_atlas"
+region_name = "us-east-1"
+session = boto3.session.Session()
+client = session.client(service_name='secretsmanager', region_name=region_name)
+secret_value = json.loads(client.get_secret_value(SecretId=secret_name)['SecretString'])
 
 reddit = praw.Reddit(
-    client_id='Wi_tu5pcSZysWpFeOHFcHA',
-    client_secret='pxNIqYLYzdjNDMa4aOn5Lruhxo-CVg',
-    user_agent='Sentiment Atlas',
-    username='RipKlutzy2899',
-    password='*Rjkq/u$7JdpaLS'
+    client_id=secret_value['client_id'],
+    client_secret=secret_value['client_secret'],
+    user_agent=secret_value['user_agent'],
+    username=secret_value['username'],
+    password=secret_value['password']
 )
+
 analyzer = SentimentIntensityAnalyzer()
 
 with open('countries.json', 'r') as file:
-    data = json.load(file)
+    countries = json.load(file)
 
-countries = data
+dynamodb = boto3.resource('dynamodb', region_name=region_name)
+table = dynamodb.Table('SentimentData')
 
-def collect_and_analyze_sentiment(country):
+def collect_analyze_and_save_sentiment(country):
     try:
-        query = country
+        query = country.country_name
         subreddit = reddit.subreddit('all')
         posts = subreddit.search(query, limit=100)
         
@@ -28,45 +38,23 @@ def collect_and_analyze_sentiment(country):
             content = post.title + ' ' + post.selftext
             sentimentCompound = analyzer.polarity_scores(content)['compound']
             sentiments.append(sentimentCompound)
+            
         if len(sentiments) == 0:
             average_sentiment = 0
         else:   
             average_sentiment = sum(sentiments) / len(sentiments)
-        print(f"Average sentiment for {country}: {average_sentiment:.4f}")
+        average_sentiment_decimal = Decimal(str(average_sentiment)).quantize(Decimal('0.0001'), rounding=ROUND_HALF_UP)
+        
+        table.put_item(
+            Item={
+                'country': country.country_id,
+                'date': datetime.now().strftime("%Y-%m-%d %H:%M:%S"),
+                'average_sentiment': average_sentiment_decimal
+            }
+        )
 
     except Exception as e:
         print(f"Error processing {country}: {e}")
 
 for country in countries:
-    collect_and_analyze_sentiment(country)
-    time.sleep(1)
-
-print("Sentiment analysis completed for all countries.")
-
-# import boto3
-# from botocore.exceptions import ClientError
-
-# def get_secret():
-
-#     secret_name = "sentiment_atlas"
-#     region_name = "us-east-1"
-
-#     # Create a Secrets Manager client
-#     session = boto3.session.Session()
-#     client = session.client(
-#         service_name='secretsmanager',
-#         region_name=region_name
-#     )
-
-#     try:
-#         get_secret_value_response = client.get_secret_value(
-#             SecretId=secret_name
-#         )
-#     except ClientError as e:
-#         # For a list of exceptions thrown, see
-#         # https://docs.aws.amazon.com/secretsmanager/latest/apireference/API_GetSecretValue.html
-#         raise e
-
-#     secret = get_secret_value_response['SecretString']
-
-#     # Your code goes here.
+    collect_analyze_and_save_sentiment(country)
