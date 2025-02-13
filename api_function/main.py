d8f60d94548e6d5b69bca33a1cf2c3ecfdbed028
Ivan <statkevich.ivan999@gmail.com>
2025-02-13 08:29:28 +0200

    Add CORS support to API handler for improved cross-origin requests

--- api_function/main.py
+++ api_function/main.py
@@ -11,10 +11,25 @@ table_name = os.getenv('TABLE_NAME')
 dynamodb = boto3.resource('dynamodb', region_name=region_name)
 table = dynamodb.Table(table_name)
 
+CORS_HEADERS = {
+    "Content-Type": "application/json",
+    "Access-Control-Allow-Origin": "https://sentiment-atlas.vercel.app",
+    "Access-Control-Allow-Methods": "OPTIONS, GET",
+    "Access-Control-Allow-Headers": "Content-Type"
+}
+
 
 def handler(event, context):
     path = event.get("rawPath", "")
     method = event.get("requestContext", {}).get("http", {}).get("method", "")
+
+    # Обробка preflight-запиту (CORS OPTIONS)
+    if method == "OPTIONS":
+        return {
+            "statusCode": 204,  # No Content
+            "headers": CORS_HEADERS,
+            "body": ""
+        }
 
     if path == "/" and method == "GET":
         try:
@@ -22,25 +37,19 @@ def handler(event, context):
             data = response.get('Items', [])
             return {
                 "statusCode": 200,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
+                "headers": CORS_HEADERS,
                 "body": json.dumps(data, default=str)
             }
         except Exception as e:
             return {
                 "statusCode": 500,
-                "headers": {
-                    "Content-Type": "application/json"
-                },
+                "headers": CORS_HEADERS,
                 "body": json.dumps({"error": str(e)})
             }
 
     else:
         return {
             "statusCode": 404,
-            "headers": {
-                "Content-Type": "application/json"
-            },
+            "headers": CORS_HEADERS,
             "body": json.dumps({"error": "Not Found"})
         }
